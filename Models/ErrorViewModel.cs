aae5dcf48db1ce3cb5c1a3e5441ab4326d692101
BUR073 <burbridgeh1@gmail.com>
2025-10-07 15:00:59 +0100

    Setup MVC structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace graphene_trace.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
