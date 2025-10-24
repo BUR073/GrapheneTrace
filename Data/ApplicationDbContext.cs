e85f2ef3b0a27384f8574687146588c0ee438b2c
BUR073 <burbridgeh1@gmail.com>
2025-10-24 15:44:28 +0100

    Update appDBContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace GrapheneTrace.Data;
 
-public class ApplicationDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
